a105e43e6557aa22d628ed15a06f317341265e72
Lucas Assis <lucaslimaassis2@gmail.com>
2026-08-24 19:55:33 -0300

    Criando meu repositorio definitivo de estudos e adicionando conversao de Titulo para JSON

---
+++ src/Principal.java
@@ -0,0 +1,28 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Principal {
+    public static void main(String[] args) throws IOException {
+        List<Titulo> listaDeTitulos = new ArrayList<>();
+
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting().create();
+
+        Titulo filme1 = new Titulo("Matrix", 1999, 90);
+        Titulo filme2 = new Titulo("Guerra do amanhã ", 2024, 120);
+
+        listaDeTitulos.add(filme1);
+        listaDeTitulos.add(filme2);
+
+        String formatado = gson.toJson(listaDeTitulos);
+
+        FileWriter escrita = new FileWriter("Filmes.json");
+        escrita.write(formatado);
+        escrita.close();
+    }
+}
---
+++ src/Titulo.java
@@ -0,0 +1,11 @@
+public class Titulo {
+    private String nome;
+    private int anoDeLancamento;
+    private int duracaoEmMinutos;
+
+    public Titulo(String nome, int anoDeLancamento, int duracaoEmMinutos) {
+        this.nome = nome;
+        this.anoDeLancamento = anoDeLancamento;
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+}
